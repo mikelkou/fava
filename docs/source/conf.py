885a3a8f3c107c91195f6063e7ff3bd4503e9ed5
Mikaela Koutrouli <81096946+mikelkou@users.noreply.github.com>
2023-06-19 18:07:57 +0200

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -30,11 +30,6 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 html_theme = 'sphinx_rtd_theme'
 html_static_path = ['_static']
 
-html_theme_options = {
-    'navigation_with_keys': True,
-    'collapse_navigation': False,
-}
-
 html_favicon = 'Supplementary\ Material/Fava_logo.png'
 html_title = project + ' version ' + release
 html_theme = 'sphinx_rtd_theme'
