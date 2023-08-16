81d6b77a62d3ae464700223cfe0fad2e65c04fd1
mikelkou <mikaela.koutrouli@cpr.ku.dk>
2023-08-16 15:34:50 +0200

    Update test_basic.py

--- tests/test_basic.py
+++ tests/test_basic.py
@@ -1,12 +1,15 @@
+import pandas as pd
 from favapy import fava
-from scvi.data import synthetic_iid
 
-adata = synthetic_iid()
+data = pd.read_csv(
+    "/Users/tgn531/Desktop/fava/data/Example_dataset_GSE75748_sc_cell_type_ec.tsv",
+    sep="\t",
+).iloc[:100, :100]
 
 
 def test_favapy():
     FAVA_network = fava.cook(
-        data=adata,
+        data=data,
         log2_normalization=True,
         hidden_layer=None,
         latent_dim=None,
