1c7be3b8049ffc626e597b97c526ebac40b0f81f
mikelkou <mikaela.koutrouli@cpr.ku.dk>
2023-08-16 11:39:39 +0200

    add tests

---
+++ tests/test_basic.py
@@ -0,0 +1,30 @@
+import scanpy as sc
+import pandas as pd
+from favapy import fava
+
+data = pd.read_csv(
+    "../data/Example_dataset_GSE75748_sc_cell_type_ec.tsv", sep="\t"
+).iloc[:100, :100]
+
+
+def test_package_has_version():
+    favapy.__version__
+
+
+def test_favapy():
+    FAVA_network = fava.cook(
+        data=data,
+        log2_normalization=True,
+        hidden_layer=None,
+        latent_dim=None,
+        epochs=10,
+        batch_size=32,
+        interaction_count=100,
+    )
+    return FAVA_network
+
+
+FAVA_network = test_favapy()
+
+# tests __repr__
+print(FAVA_network)
